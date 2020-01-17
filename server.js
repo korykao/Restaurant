45ccbc9e446c42252317ef03c8b85b2195a9e77f
Michelle Park <parkm37@yahoo.com>
2020-01-16 19:49:03 -0800

    complete maybe

--- server.js
+++ server.js
@@ -3,10 +3,14 @@ var path = require("path");
 
 var app = express();
 var PORT = process.env.PORT || 6969;
+var tableAmount = 5;
+var waitlistAmount = 5;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+var reservations = [];
+var waitlist = [];
 
 // Routes
 app.get("/", function(req, res) {
@@ -22,9 +26,35 @@ app.get("/reserve", function(req, res) {
 });
 
 // Displays all characters
-//app.get("/api/tables", function(req, res) {
-//  return res.json(characters);
-//});
+app.get("/api/tables", function(req, res) {
+  return res.json(reservations);
+});
+
+app.get("/api/waitlist", function(req, res)
+{
+  return res.json(waitlist);
+});
+
+app.post("/api/tables", function(req, res)
+{
+  var newReservation = req.body;
+  if(tableAmount > 0)
+  {
+    reservations.push(newReservation);
+    tableAmount--;
+    res.json(newReservation);
+  }
+  else if(waitlistAmount > 0)
+  {
+    waitlist.push(newReservation);
+    waitlistAmount--;
+    res.json(newReservation);
+  }
+  else
+  {
+    res.json(false);
+  }
+})
 
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
